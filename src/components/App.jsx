03286609104bdd367ef48a29df62b4d995e17bc2
Olga Polishchuk <olgapolichuk@ukr.net>
2023-03-24 20:14:42 +0200

    init commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,103 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import { Section } from './Section/Section';
+import { ContactsList } from './ContactsList/ContactsList';
+import { PhoneBook } from './PhoneBook/PhoneBook';
+import { Filter } from './Filter/Filter';
+import 'react-notifications/lib/notifications.css';
+import {
+  NotificationContainer,
+  NotificationManager,
+} from 'react-notifications';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({contacts: parsedContacts})
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+    }
+  }
+
+  addContact = ({ name, number }) => {
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    const checkName = this.state.contacts.find(
+      contact => contact.name === name
+    );
+    if (checkName) {
+      NotificationManager.warning(`${name} is already in contacts`)
+      return;
+    }
+    this.setState(({ contacts }) => ({
+      contacts: [contact, ...contacts]
+    }));
+    NotificationManager.success(`Contact ${name} added successfully`)
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+    NotificationManager.error('One contact has been deleted');
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getVisibleContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+    const totalcontactsCount = contacts.length;
+    const visibleContacts = this.getVisibleContacts();
+
+    return (
+      <>
+        <Section title="Phonebook">
+          <PhoneBook onSubmit={this.addContact} />
+        </Section>
+        <Section title="Contacts">
+          <Filter
+            value={filter}
+            onChange={this.changeFilter}
+            totalcontactsCount={totalcontactsCount}
+          />
+
+          <ContactsList
+            contacts={visibleContacts}
+            onDeleteContact={this.deleteContact}
+          />
+        </Section>
+        <NotificationContainer />
+      </>
+    );
+  }
+}
+
+export default App;
